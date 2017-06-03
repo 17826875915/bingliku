b769f78feb30260518321e81838cc1e516c61d4c
mao <Administrator@YOS-01701121031>
2017-06-03 19:48:50 +0800

    删除了修改医生的按钮，对管理医生界面进行了排版

--- bingliku/管理医生.Designer.cs
+++ bingliku/管理医生.Designer.cs
@@ -38,7 +38,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -59,6 +58,7 @@
             this.Column5});
             this.dataGridView1.Location = new System.Drawing.Point(2, 36);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(549, 224);
             this.dataGridView1.TabIndex = 0;
@@ -97,7 +97,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(229, 288);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 29);
+            this.button1.Size = new System.Drawing.Size(108, 29);
             this.button1.TabIndex = 1;
             this.button1.Text = "查询";
             this.button1.UseVisualStyleBackColor = true;
@@ -105,9 +105,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(343, 288);
+            this.button2.Location = new System.Drawing.Point(375, 288);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(68, 29);
+            this.button2.Size = new System.Drawing.Size(114, 29);
             this.button2.TabIndex = 2;
             this.button2.Text = "显示所有";
             this.button2.UseVisualStyleBackColor = true;
@@ -115,9 +115,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(229, 350);
+            this.button3.Location = new System.Drawing.Point(229, 343);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(81, 29);
+            this.button3.Size = new System.Drawing.Size(108, 29);
             this.button3.TabIndex = 3;
             this.button3.Text = "删除该医生信息";
             this.button3.UseVisualStyleBackColor = true;
@@ -125,22 +125,13 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(346, 350);
+            this.button4.Location = new System.Drawing.Point(375, 341);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(65, 28);
+            this.button4.Size = new System.Drawing.Size(111, 31);
             this.button4.TabIndex = 4;
             this.button4.Text = "新增医生";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(441, 346);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(68, 32);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "修改医生信息";
-            this.button5.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -183,7 +174,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -209,7 +199,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
